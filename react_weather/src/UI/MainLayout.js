3a10c7907e7b340f01262c4770024f1c62eac2b6
Konrad Dominowski <zehkan@gmail.com>
2023-05-07 17:10:24 +0200

    files

--- react_weather/src/UI/MainLayout.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import classes from './MainLayout.module.css'
-
-export default function Mainlayout({ children }) {
-	return (
-		<div className={ classes.layout }>{ children }</div>
-	)
-}
